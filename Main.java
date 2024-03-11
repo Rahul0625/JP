d0a97385fb90368310914d2229742530653616b9
Rahul0625 <rahultrickstar2017@gmail.com>
2024-03-11 10:45:00 +0530

    Build Done 3

--- Main.java
+++ Main.java
@@ -2,5 +2,6 @@ public class Main{
 	public static void main(String[] args){
 		System.out.println("Hello World");
 		System.out.println("Auto triggered successfully");
+		System.out.println("Build 3");
 		}
 }
